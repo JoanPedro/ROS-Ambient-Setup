ead8a5b827d542eda5ea557425b51dcff611b9d2
Joan Pedro <jpos.senai@gmail.com>
2020-03-21 12:50:09 -0300

    Adiciona Pasta Devel

---
+++ devel/.private/hrwros_msgs/share/gennodejs/ros/hrwros_msgs/msg/_index.js
@@ -0,0 +1,44 @@
+
+"use strict";
+
+let BoxHeightInformation = require('./BoxHeightInformation.js');
+let TargetToolPoses = require('./TargetToolPoses.js');
+let RobotTrajectories = require('./RobotTrajectories.js');
+let CounterWithDelayActionResult = require('./CounterWithDelayActionResult.js');
+let CounterWithDelayResult = require('./CounterWithDelayResult.js');
+let CounterWithDelayGoal = require('./CounterWithDelayGoal.js');
+let CounterWithDelayAction = require('./CounterWithDelayAction.js');
+let ObjectDetection = require('./ObjectDetection.js');
+let CounterWithDelayActionGoal = require('./CounterWithDelayActionGoal.js');
+let CounterWithDelayActionFeedback = require('./CounterWithDelayActionFeedback.js');
+let CounterWithDelayFeedback = require('./CounterWithDelayFeedback.js');
+let SensorInformation = require('./SensorInformation.js');
+let CounterWithDelayActionResult = require('./CounterWithDelayActionResult.js');
+let CounterWithDelayResult = require('./CounterWithDelayResult.js');
+let CounterWithDelayGoal = require('./CounterWithDelayGoal.js');
+let CounterWithDelayAction = require('./CounterWithDelayAction.js');
+let CounterWithDelayActionGoal = require('./CounterWithDelayActionGoal.js');
+let CounterWithDelayActionFeedback = require('./CounterWithDelayActionFeedback.js');
+let CounterWithDelayFeedback = require('./CounterWithDelayFeedback.js');
+
+module.exports = {
+  BoxHeightInformation: BoxHeightInformation,
+  TargetToolPoses: TargetToolPoses,
+  RobotTrajectories: RobotTrajectories,
+  CounterWithDelayActionResult: CounterWithDelayActionResult,
+  CounterWithDelayResult: CounterWithDelayResult,
+  CounterWithDelayGoal: CounterWithDelayGoal,
+  CounterWithDelayAction: CounterWithDelayAction,
+  ObjectDetection: ObjectDetection,
+  CounterWithDelayActionGoal: CounterWithDelayActionGoal,
+  CounterWithDelayActionFeedback: CounterWithDelayActionFeedback,
+  CounterWithDelayFeedback: CounterWithDelayFeedback,
+  SensorInformation: SensorInformation,
+  CounterWithDelayActionResult: CounterWithDelayActionResult,
+  CounterWithDelayResult: CounterWithDelayResult,
+  CounterWithDelayGoal: CounterWithDelayGoal,
+  CounterWithDelayAction: CounterWithDelayAction,
+  CounterWithDelayActionGoal: CounterWithDelayActionGoal,
+  CounterWithDelayActionFeedback: CounterWithDelayActionFeedback,
+  CounterWithDelayFeedback: CounterWithDelayFeedback,
+};
